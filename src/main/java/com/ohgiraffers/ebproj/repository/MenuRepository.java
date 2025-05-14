84c483ab48212e4dda8446b7062532d9a4530384
SungMin-hub <sungmin806@gmail.com>
2025-05-14 10:14:37 +0900

    get menuCode

---
+++ src/main/java/com/ohgiraffers/ebproj/repository/MenuRepository.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.ebproj.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ohgiraffers.ebproj.entity.Menu;
+
+public interface MenuRepository extends JpaRepository<Menu, Integer> {
+
+}
