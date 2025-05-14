6f2a8982de0bc953834f0d96f07173878fe8cef4
SungMin-hub <sungmin806@gmail.com>
2025-05-14 10:56:36 +0900

    pr test

--- src/main/java/com/ohgiraffers/ebproj/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/ebproj/controller/MenuController.java
@@ -20,7 +20,7 @@ public class MenuController {
 
 	@GetMapping("/health")
 	public String healthCheck() {
-		return "I'm alive1234!";
+		return "I'm alive12345!";
 	}
 
 	@GetMapping("/menus/{menuCode}")
